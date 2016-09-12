70e83445c70b1116baccd49a597db458b71a86ec
Jack Hodgkiss <jackhodgkiss@outlook.com>
2016-09-12 12:31:22 +0100

    popular stories list has been isolated.

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ def main():
     response = requests.get(url)  # send a GET request using the specified URL and save the resulting response.
 
     soup = BeautifulSoup(response.content, "html.parser")  # Create a BeautifulSoup object using the requested HTML doc.
-    print(soup.prettify())  # Print the HTML document in a more readable format.
+    # print(soup.prettify())  # Print the HTML document in a more readable format.
+
+    most_popular = soup.find_all("ul", {"class": "most-popular-page__list"})  # Get the <ul> containing popular stories.
+    print(most_popular)  # Print the <ul> and containing child elements.
 
 if __name__ == "__main__":
     main()
