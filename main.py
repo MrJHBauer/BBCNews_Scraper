35b1ee07cbda3f6f6720d83f470700d5cab01b33
Jack Hodgkiss <jackhodgkiss@outlook.com>
2016-09-12 12:26:45 +0100

    response has been handed to BS4

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def main():
     url = "http://www.bbc.co.uk/news/popular/read"  # URL of the page where 10 most popular news stories are available.
     response = requests.get(url)  # send a GET request using the specified URL and save the resulting response.
 
-    print(response.text)  # Print the responses text.
+    soup = BeautifulSoup(response.content, "html.parser")  # Create a BeautifulSoup object using the requested HTML doc.
+    print(soup.prettify())  # Print the HTML document in a more readable format.
 
 if __name__ == "__main__":
     main()
