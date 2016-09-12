1a21d444b60b60fa00109ece055e2375daa318bf
Jack Hodgkiss <jackhodgkiss@outlook.com>
2016-09-12 12:13:06 +0100

    popular news page fetched via requests

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import requests
+
+
 def main():
-    pass
+    url = "http://www.bbc.co.uk/news/popular/read"
+    response = requests.get(url)
+
+    print(response.text)
 
 if __name__ == "__main__":
     main()
