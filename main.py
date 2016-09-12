069b939162079aee5131bff48bbbf89a316f3882
Jack Hodgkiss <jackhodgkiss@outlook.com>
2016-09-12 15:03:56 +0100

    title and link are now stored within dict.

--- main.py
+++ main.py
@@ -10,7 +10,17 @@ def main():
     # print(soup.prettify())  # Print the HTML document in a more readable format.
 
     most_popular = soup.find_all("a", {"class": "most-popular-page-list-item__link"})  # Get the <a> tags with stories.
-    print(most_popular)  # Print the anchor tags and containing child elements.
+    # print(most_popular)  # Print the anchor tags and containing child elements.
+
+    stories = {}  # Create a dict to store the title and links for popular news stories. Order won't be maintained.
+
+    for anchor in most_popular:
+        link = anchor.get("href")
+        title = anchor.contents[3].text
+        stories[title] = link  # Save the title and link of the story and key, value pair within the dict.
+
+    for title, link in stories.items():
+        print("Title: {} \nURL: http://www.bbc.co.uk{}".format(title, link))  # Print the title and link to console.
 
 if __name__ == "__main__":
     main()
