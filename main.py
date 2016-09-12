03d47e318407313923fdd080207b2acfdb6efef5
Jack Hodgkiss <jackhodgkiss@outlook.com>
2016-09-12 12:20:30 +0100

    Commented

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import requests
 
 
 def main():
-    url = "http://www.bbc.co.uk/news/popular/read"
-    response = requests.get(url)
+    url = "http://www.bbc.co.uk/news/popular/read"  # URL of the page where 10 most popular news stories are available.
+    response = requests.get(url)  # send a GET request using the specified URL and save the resulting response.
 
-    print(response.text)
+    print(response.text)  # Print the responses text.
 
 if __name__ == "__main__":
     main()
