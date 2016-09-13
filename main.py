b8bcf80acca2171f9a17001461983f5da0984f91
Jack Hodgkiss <jackhodgkiss@outlook.com>
2016-09-13 15:47:32 +0100

    main.py cleaned up.

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ def main():
     response = requests.get(url)  # send a GET request using the specified URL and save the resulting response.
 
     soup = BeautifulSoup(response.content, "html.parser")  # Create a BeautifulSoup object using the requested HTML doc.
-    # print(soup.prettify())  # Print the HTML document in a more readable format.
 
     most_popular = soup.find_all("a", {"class": "most-popular-page-list-item__link"})  # Get the <a> tags with stories.
-    # print(most_popular)  # Print the anchor tags and containing child elements.
 
     stories = {}  # Create a dict to store the title and links for popular news stories. Order won't be maintained.
 
@@ -18,9 +16,6 @@ def main():
         link = anchor.get("href")
         title = anchor.contents[3].text
         stories[title] = link  # Save the title and link of the story and key, value pair within the dict.
-
-    # for title, link in stories.items():
-    #    print("Title: {} \nURL: http://www.bbc.co.uk{}".format(title, link))  # Print the title and link to console.
 
     for story in stories:
         url = stories[story]  # Get the URL for the current story.
